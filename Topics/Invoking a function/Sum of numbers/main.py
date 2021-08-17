69f550b17b2d0b8659b06f25795175389f06af04
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2021-08-17 18:06:34 +0200

    Practice

---
+++ Topics/Invoking a function/Sum of numbers/main.py
@@ -0,0 +1,6 @@
+x = int(input())
+y = int(input())
+# the variables `x` and `y` are defined, so just print their sum
+
+numbers = [x,y]
+print(sum(numbers))
---
+++ Topics/Invoking a function/Young and beautiful/main.py
@@ -0,0 +1,6 @@
+jack_age = int(input())
+alex_age = int(input())
+lana_age = int(input())
+
+age = [jack_age, alex_age, lana_age]
+print(min(age))
